356ab5c34ca3f7cd8ecd9a69cc7db4727c046107
João Medeiros <joaopvmedeiross@gmail.com>
2025-04-28 23:51:55 -0300

    finalmente

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Importa o express
 import express from "express";
 import cors from "cors";
-const app = express();
 
-// Define a porta que a API irá escutar
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 // Define uma rota GET para /datetime
-app.get('/datetime', (req, res) => {
-  const now = new Date();
-  
-  res.json({
-    date: now.toLocaleDateString(), // formato de data
-    time: now.toLocaleTimeString()   // formato de hora
+app.get('/', (req, res) => {
+   res.json({
+    date: new Date().toLocaleString('pt-br'), // formato de data
+    status: 'API no Render funcionando!' 
   });
 });
 
