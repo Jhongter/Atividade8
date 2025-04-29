a6e2de00cbf9a3dafc01bd7c4e0710bc964ef16d
João Medeiros <joaopvmedeiross@gmail.com>
2025-04-28 23:43:36 -0300

    Data e hora

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');
+// Importa o express
+import express from "express";
+import cors from "cors";
 const app = express();
 
-// Define a porta (Render usa process.env.PORT)
-const PORT = process.env.PORT || 3000;
+// Define a porta que a API irá escutar
+const PORT = 3000;
 
-// Rota principal para retornar a data e hora atuais
+// Define uma rota GET para /datetime
 app.get('/datetime', (req, res) => {
   const now = new Date();
+  
   res.json({
-    date: now.toLocaleDateString(),
-    time: now.toLocaleTimeString(),
-    iso: now.toISOString()
+    date: now.toLocaleDateString(), // formato de data
+    time: now.toLocaleTimeString()   // formato de hora
   });
 });
 
 // Inicia o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
